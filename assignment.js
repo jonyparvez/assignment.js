e65d268155eef2cd247143cb5b1ea33dde72d830
Johnnyparvez <jony44pbn@gmail.com>
2021-01-25 00:10:32 +0600

    added git link

---
+++ assignment.js
@@ -0,0 +1,12 @@
+//https://github.com/jonyparvez/assignment.js/blob/main/assignment.js
+
+
+//kilometerToMeter
+
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+
+}
+var meter = kilometerToMeter(15);
+console.log(meter);
